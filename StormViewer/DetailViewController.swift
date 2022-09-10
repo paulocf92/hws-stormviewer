cfd79be0d0d4b310886f3fb3313c0dbe9bfde25b
Paulo Filho <pcf.feat@gmail.com>
2022-09-10 14:04:09 -0400

    study: add titles to view controllers

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -14,6 +14,8 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = selectedImage
 
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
